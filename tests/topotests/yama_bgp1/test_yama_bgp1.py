1239de716d3a4b518f9ae6b3a417e75c294cf774
yushoyamaguchi <ysh.824@outlook.jp>
2022-04-14 23:27:17 +0900

    yama_bgp1

---
+++ tests/topotests/yama_bgp1/test_yama_bgp1.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import json
+import pytest
+import functools
+
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(CWD, "../"))
+
+
+from lib import topotest
+from lib.topogen import Topogen, TopoRouter, get_topogen
+
+pytestmark = [pytest.mark.bgpd]
+
+
+def build_topo(tgen):
+    tgen.add_router("r1")
+    tgen.add_router("r2")
+    tgen.add_router("c1")
+    tgen.add_router("c2")
+    """
+    for routern in range(1, 3):
+        tgen.add_router("r{}".format(routern))
+    """
+    tgen.add_link(tgen.gears["r1"], tgen.gears["r2"], "r1-eth0", "r2-eth0")
+    tgen.add_link(tgen.gears["c1"], tgen.gears["r1"], "c1-eth0", "r1-eth1")
+    tgen.add_link(tgen.gears["c2"], tgen.gears["r2"], "c2-eth0", "r2-eth1")
+
+
+def setup_module(mod):
+    tgen = Topogen(build_topo, mod.__name__)
+    tgen.start_topology()
+
+    router_list = tgen.routers()
+
+    for i, (rname, router) in enumerate(router_list.items(), 1):
+        router.load_config(
+            TopoRouter.RD_ZEBRA, os.path.join(CWD, "{}/zebra.conf".format(rname))
+        )
+        router.load_config(
+            TopoRouter.RD_BGP, os.path.join(CWD, "{}/bgpd.conf".format(rname))
+        )
+    #linuxコマンドによる設定はここでやる(例は下の通り)
+    #tgen.gears["r1"].run("ip link add vrf10 type vrf table 10")
+    tgen.start_router()
+
+
+def teardown_module(mod):
+    tgen = get_topogen()
+    tgen.stop_topology()
+
+"""
+ここにテスト内容を記述
+"""
+def test_bgp_default_originate_route_map():
+    tgen = get_topogen()
+
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+
+
+if __name__ == "__main__":
+    args = ["-s"] + sys.argv[1:]
+    sys.exit(pytest.main(args))
